4378bacf4184dfa1ef0059f9ea21cbe484835b44
SarpArslan77 <157630184+SarpArslan77@users.noreply.github.com>
2025-07-05 18:58:12 +0200

    Update main.py

--- Math Simulations/Collatz Conjecture/main.py
+++ Math Simulations/Collatz Conjecture/main.py
@@ -1,21 +1,17 @@
 
-#TODO: finish the plot all leading digit graph at once
-#TODO: try yo do the general animated version, on the same figure. rn it is being closed / opened for every sequence. it should be smoother
-#TODO: add the other data graphs from the video as noted on the notebook
-
-
 from animate_collatz_conjecture_datas import (
     animate_each_value, 
     plot_all_value, 
-    log_value_of_one_number,
-    animate_leading_digit
+    plot_log_value_of_one_number,
+    animate_leading_digit,
+    plot_all_leading_digit,
+    plot_highest_reached_number
 )
 from collatz_conjecture_functions import generate_hailstone_sequences
 
-
 num: int = 27
-start_value: int = 1
-end_value: int = 10000
+start_value: int = 50
+end_value: int = 100
 detrended: bool = True
 
 # Call of the main function
@@ -24,5 +20,6 @@ hailstone_numbers, total_stoppage_time = generate_hailstone_sequences(start_valu
 #plot_all_value(start_value, end_value, hailstone_numbers, total_stoppage_time)
 #animate_each_value(start_value, hailstone_numbers, total_stoppage_time)
 #log_value_of_one_number(num, hailstone_numbers, total_stoppage_time, detrended)
-animate_leading_digit(start_value, end_value, hailstone_numbers)
-
+#animate_leading_digit(start_value, end_value, hailstone_numbers)
+#plot_all_leading_digit(start_value, end_value, hailstone_numbers)
+plot_highest_reached_number(start_value, end_value, hailstone_numbers)
