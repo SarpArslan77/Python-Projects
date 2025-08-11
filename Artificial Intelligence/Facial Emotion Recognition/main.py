0bca4451eae52e1d7891c934d3d2ed2ad53971d5
SarpArslan77 <157630184+SarpArslan77@users.noreply.github.com>
2025-08-11 11:31:40 +0200

    Update main.py

--- Artificial Intelligence/Facial Emotion Recognition/main.py
+++ Artificial Intelligence/Facial Emotion Recognition/main.py
@@ -4,118 +4,13 @@ import numpy as np
 import torch
 import torch.nn as nn
 from sklearn.metrics import classification_report
+import intel_extension_for_pytorch as ipex
+
 from data import (
     test_loader,
     train_loader,
     val_loader,
 )
-import intel_extension_for_pytorch as ipex
-
-# Neural Network implementation
-"""
-from nn import (
-    NeuralNetwork,
-    input_size,
-    hidden_node_amount,
-    num_classes,
-    num_epochs,
-    batch_size,
-    learning_rate,
-    device
-)
-
-# Create the NN-Model.
-model = NeuralNetwork(input_size, hidden_node_amount, num_classes).to(device)
-criterion = nn.CrossEntropyLoss() # includes Softmax Activation
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-
-# Training Loop
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(training_loader):
-        # Reshape the images from (batch_size, 1, 48, 48) to (batch_size, 2304)
-        images = images.reshape(-1, 48*48).to(device)
-        labels = labels.to(device)
-
-        # Forward Pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-
-        # Backward and Optimize
-        optimizer.zero_grad() # Clear gradients from the previous step.
-        loss.backward() # Compute the gradients.
-        optimizer.step() # Update the weights and biases
-    
-    print(f"Epoch [{epoch}/{num_epochs-1}], Loss: {loss.item():.4f}")
-print("\nFinished Training!")
-
-# Test Loop
-print("\nStarting with the Test.")
-model.eval()
-with torch.no_grad(): 
-# We use torch.no_grad() to save memory and computations, as we don't need gradients here
-    total_n_correct: int = 0
-    total_n_samples: int = 0
-    # TP: True Positive, TN: True Negative, FP: False Positive, FN: False Negative
-    emotion_counts: list = [
-        [0 for _ in range(2)] for _ in range(7)
-    ] # Is built like: [[TP_angry, angry_count], [TP_disgust, disgust_count], ...]
-
-    for images, labels in test_loader:
-        images = images.reshape(-1, 48*48).to(device)
-        labels = labels.to(device) # Shape: (512)
-        outputs = model(images)
-
-        _, predicted = torch.max(outputs.data, 1)
-
-        # Calculate the overall accuracy. 
-        total_n_samples += labels.size(0)
-        total_n_correct += (predicted == labels).sum().item()
-
-        # Calculate the precision for each emotion.
-        emotion_counts[0][0] += torch.logical_and(labels == 0, predicted == 0).sum().item()
-        emotion_counts[0][1] += (predicted == 0).sum().item()
-        emotion_counts[1][0] += torch.logical_and(labels == 1, predicted == 1).sum().item()
-        emotion_counts[1][1] += (predicted == 1).sum().item()
-        emotion_counts[2][0] += torch.logical_and(labels == 2, predicted == 2).sum().item()
-        emotion_counts[2][1] += (predicted == 2).sum().item()
-        emotion_counts[3][0] += torch.logical_and(labels == 3, predicted == 3).sum().item()
-        emotion_counts[3][1] += (predicted == 3).sum().item()
-        emotion_counts[4][0] += torch.logical_and(labels == 4, predicted == 4).sum().item()
-        emotion_counts[4][1] += (predicted == 4).sum().item()
-        emotion_counts[5][0] += torch.logical_and(labels == 5, predicted == 5).sum().item()
-        emotion_counts[5][1] += (predicted == 5).sum().item()
-        emotion_counts[6][0] += torch.logical_and(labels == 6, predicted == 6).sum().item()
-        emotion_counts[6][1] += (predicted == 6).sum().item()
-        
-        print(emotion_counts[0][1])
-        print(emotion_counts[1][1])
-        print(emotion_counts[2][1])
-        print(emotion_counts[3][1])
-        print(emotion_counts[4][1])
-        print(emotion_counts[5][1])
-        print(emotion_counts[6][1])
-        print()
-
-    accuracy = round(total_n_correct/total_n_samples * 100, 2)
-    print(f"\nOverall accuracy on the test set: {accuracy} %")
-
-    precision_angry: float = round(emotion_counts[0][0] / emotion_counts[0][1], 2)
-    precision_disgust: float = round(emotion_counts[1][0] / emotion_counts[1][1], 2)
-    precision_fear: float = round(emotion_counts[2][0] / emotion_counts[2][1], 2)
-    precision_happy: float = round(emotion_counts[3][0] / emotion_counts[3][1], 2)
-    precision_neutral: float = round(emotion_counts[4][0] / emotion_counts[4][1], 2)
-    precision_sad: float = round(emotion_counts[5][0] / emotion_counts[5][1], 2)
-    precision_surprise: float = round(emotion_counts[6][0] / emotion_counts[6][1], 2)
-    print(f"\nPrecision on the Emotions:")
-    print(f"angry: {precision_angry} %")
-    print(f"disgust: {precision_disgust} %")
-    print(f"fear: {precision_fear} %")
-    print(f"happy: {precision_happy} %")
-    print(f"neutral: {precision_neutral} %")
-    print(f"sad: {precision_sad} %")
-    print(f"surprise: {precision_surprise} %")
-"""
-
 from cnn import (
     device,
     model,
@@ -125,7 +20,7 @@ from cnn import (
 )
 
 # Hyperparameters
-NUM_EPOCHS: int = 10
+NUM_EPOCHS: int = 50
 
 # Apply IPEX optimizations to the model and optimizer
 model, optimizer = ipex.optimize(model, optimizer=optimizer)
@@ -133,39 +28,55 @@ model, optimizer = ipex.optimize(model, optimizer=optimizer)
 # Training Loop
 print("\nStarting Training!")
 for epoch in range(NUM_EPOCHS):
+    print(f"\nEpoch : {epoch+1}/{NUM_EPOCHS}")
     model.train()
     running_loss: float = 0.0 # Tracking loss for optimizing the learning rate.
+    n_correct: int = 0
+    n_total: int = 0
     for i, (images, labels) in enumerate(train_loader):
-        print(i)
         images, labels = images.to(device), labels.to(device)
 
         # Forward pass
         outputs = model(images)
         loss = criterion(outputs, labels)
 
+        # Calculate the accuracy.
+        _, predicted = torch.max(outputs.data, 1)
+        n_total += labels.size(0)
+        # All tensors have to be on the same device.
+        n_correct += (predicted.cpu() == labels.cpu()).sum().item()
+
         # Backward pass and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        # Update the loss
+        # Update the loss.
         running_loss += loss.item() * images.size(0)
 
+    train_acc: float = 100.0 * n_correct / n_total
     epoch_train_loss: float = running_loss / len(train_loader.dataset)
+    print(f"\nTrain accuracy : {train_acc:.2f}% | Train Loss: {epoch_train_loss:.4f}")
 
     # Validation Phase
     model.eval()
     val_loss: float = 0.0
+    val_correct: int = 0
+    val_total: int = 0
     with torch.no_grad():
         for images, labels in val_loader:
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             loss = criterion(outputs, labels)
             val_loss += loss.item() * images.size(0)
-    
-    epoch_val_loss: float = val_loss / len(val_loader.dataset)
 
-    print(f"Epoch {epoch+1}/{NUM_EPOCHS} | Train Loss: {epoch_train_loss:.4f} | Val Loss: {epoch_val_loss:.4f}")
+            _, predicted = torch.max(outputs.data, 1)
+            val_total += labels.size(0)
+            val_correct += (predicted.cpu() == labels.cpu()).sum().item()
+    
+    val_acc: float = 100.0 * val_correct / val_total
+    epoch_val_loss: float = val_loss / len(val_loader.dataset)
+    print(f"Validation accuracy : {val_acc:.2f}% | Val Loss: {epoch_val_loss:.4f}")
 
     # Step the scheduler with validation loss
     scheduler.step(epoch_val_loss)
@@ -193,7 +104,6 @@ with torch.no_grad():
     n_correct: int = 0
     n_samples: int = 0
     for j, (images, labels) in enumerate(test_loader):
-        print(j)
         images, labels = images.to(device), labels.to(device)
 
         outputs = model(images)
