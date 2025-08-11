fdb61ce718d59a401bee56996eda1691d6b95efb
SarpArslan77 <157630184+SarpArslan77@users.noreply.github.com>
2025-08-11 11:31:22 +0200

    Update cnn.py

--- Artificial Intelligence/Facial Emotion Recognition/cnn.py
+++ Artificial Intelligence/Facial Emotion Recognition/cnn.py
@@ -1,63 +1,50 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import intel_extension_for_pytorch as ipex
-
 
 device = torch.device("xpu" if torch.xpu.is_available() else "cpu")
 print(f"\nUsing device: {device}") # <-- ADD THIS LINE
 
 # Hyperparameters
-LEARNING_RATE: float = 0.001
+LEARNING_RATE: float = 1e-3
 
 class ConvNet(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, num_class: int = 7) -> None:
         super(ConvNet, self).__init__()
         # Input shape: (1, 48, 48)
 
         self.conv_layer1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.conv_layer2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2) # Output shape: (64, 24, 24)
         )
         self.conv_layer3 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=128),
             nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.conv_layer4 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=128),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2) # Output shape: (128, 12, 12)
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.conv_layer5 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=256),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(),
-        )
-        self.conv_layer6 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2) # Output shape: (256, 6, 6)
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
         # After two max pools, the 48x48 image is now 12x12
         self.fc_layer = nn.Sequential(
-            nn.Linear(in_features=256*6*6, out_features=1024),
+            nn.Linear(in_features=128*1*1, out_features=256),
             nn.ReLU(),
             nn.Dropout(),
-            nn.Linear(in_features=1024, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(512, 7) # 7 classes = 7 emotions
+            nn.Linear(in_features=256, out_features=num_class),
         )
 
     def forward(self, x):
@@ -66,18 +53,16 @@ class ConvNet(nn.Module):
         x = self.conv_layer3(x)
         x = self.conv_layer4(x)
         x = self.conv_layer5(x)
-        x = self.conv_layer6(x)
         x = x.view(x.size(0), -1)
-        x = self.fc_layer(x)
 
-        return x
+        return self.fc_layer(x)
     
 # Convolutional Neural Network implementation
 model = ConvNet().to(device)
 
 # Define Loss Function and Optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9)
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 # Scheduler Definition
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(
