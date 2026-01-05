284feeb4969001b07510bb25d7fe0a7aac996918
SarpArslan77 <157630184+SarpArslan77@users.noreply.github.com>
2026-01-05 09:09:05 +0100

    Refactor graph.py for improved structure and clarity

--- Artificial Intelligence/Long-Short Term Memory/Battery Health/graph.py
+++ Artificial Intelligence/Long-Short Term Memory/Battery Health/graph.py
@@ -15,7 +15,7 @@
 from dataclasses import dataclass
 import os
 
-import matplotlib.gridspec as gridspec
+from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
@@ -23,42 +23,10 @@ import mplcursors
 import numpy as np
 from numpy.typing import NDArray
 
-@dataclass
-class ConfigHistoryPlotter:
-    """
-    Configuration dataclass for the HistoryPlotter visualization tool.
-
-    Attributes:
-        train_time (tuple[float, float, float]): The duration of the training session
-            formatted as (Hours, Minutes, Seconds).
-    """
-    
-    train_time: tuple[float, float, float]
-
-    def __post_init__(self) -> None:
-        # Unpack the times.
-        train_hour, train_min, train_sec = self.train_time
-
-        # - train_hour
-        if not isinstance(train_hour, float):
-            raise TypeError(f"train_hour must be an float, got {type(train_hour).__name__}.")
-        if train_hour < 0:
-            raise ValueError(f"train_hour must be non-negative, got {train_hour}.")
-        # - train_min
-        if not isinstance(train_min, float):
-            raise TypeError(f"train_min must be an float, got {type(train_min).__name__}.")
-        if train_min < 0:
-            raise ValueError(f"train_min must be non-negative, got {train_min}.")
-        # - train_sec
-        if not isinstance(train_sec, float):
-            raise TypeError(f"train_sec must be an float, got {type(train_sec).__name__}.")
-        if train_sec < 0:
-            raise ValueError(f"train_sec must be non-negative, got {train_sec}.")
-
 # Define the custom type hints.
 History = tuple[NDArray, NDArray]
 
-class HistoryPlotter():
+class Plotter():
     """
     Visualization engine for comparing training and validation metrics.
 
@@ -66,29 +34,6 @@ class HistoryPlotter():
     Handles the alignment of validation data (which often has fewer data points)
     to match the training epochs x-axis.
     """
-    
-    def __init__(
-            self,
-            config_history_plotter: ConfigHistoryPlotter
-    ) -> None:
-        # Define the input parameters.
-        self.cfg: ConfigHistoryPlotter = config_history_plotter
-
-        # Unpack the training time for the title.
-        train_hour, train_min, train_sec = self.cfg.train_time
-        
-        # Define the figure.
-        self.fig: Figure = plt.figure(figsize=(10, 10))
-        self.fig.suptitle(f"Training Time: {int(train_hour):02d}:{int(train_min):02d}:{int(train_sec):02d}")
-
-        # Define the gridspace.
-        self.gs = gridspec.GridSpec(
-            nrows = 2,
-            ncols = 2,
-            figure = self.fig
-        )
-        # Adjust the spacing between the subplots.
-        self.gs.update(wspace=0.3)
 
     def _upsample_data(
             self,
@@ -140,7 +85,31 @@ class HistoryPlotter():
 
         return expanded_val_data
 
-    def _plot_subplot(
+    def _generate_x_axis_range(
+            self,
+            data: NDArray
+    ) -> NDArray:
+        """
+        Creates a sequential integer array to represent the X-axis.
+
+        Args:
+            data (NDArray): The data array whose length determines the range.
+
+        Returns:
+            NDArray: An array [1, 2, ..., N] where N is the length of data.
+        """
+        # Get the length of data.
+        num_epoch: int = len(data)
+
+        # Create the range.
+        epoch_range: NDArray = np.arange(
+            start = 0,
+            stop = num_epoch
+        )
+
+        return epoch_range
+
+    def _plot_history_subplot(
             self,
             ax: Axes,
             data: tuple[NDArray, NDArray],
@@ -171,12 +140,7 @@ class HistoryPlotter():
         )
 
         # 3. Create the x-axis vector.
-        num_epoch: int = len(train_data)
-
-        epoch_range: NDArray = np.arange(
-            start = 1,
-            stop = num_epoch + 1
-        )
+        epoch_range: NDArray = self._generate_x_axis_range(data=train_data)
 
         # 4. Plot the training and validation data.
         train_line, = ax.plot(
@@ -205,95 +169,194 @@ class HistoryPlotter():
         # 6. Add a cursor to the graph.
         mplcursors.cursor([train_line, val_line])
 
-    def _save_plots(
+    def _save_graph(
             self,
-            trial_folder_path: str
+            trial_folder_path: str,
+            image_name: str,
     ) -> None:
         """
-        Saves the generated figure to the specified directory.
-
-        The file is saved as 'loss_histories.png'.
+        Saves the generated figure to the specified directory with a specified name.
 
         Args:
             trial_folder_path (str): The directory path where the image should be saved.
+            image_name  (str): The name, under which the image will be saved as .png file.
         """
 
         try: # Try to create a graph for the losses.
-            graph_path: str = os.path.join(trial_folder_path, "loss_histories.png")
+            graph_path: str = os.path.join(trial_folder_path, f"{image_name}.png")
             plt.savefig(graph_path)
             print(f"\nThe Graph {graph_path} has been created succesfully!")
         except Exception as e:
             print(f"\n!!! The saving of ratio graph has failed !!!")
             print(f"Error: {e}")
 
-    def plot_history(
+    def plot_training_results(
             self,
+            train_time: tuple[float, float, float],
             histories: tuple[History, History, History, History],
-            plot_colors: tuple[str, str],
-            plot_linestyles: tuple[str, str],
+            colors: tuple[str, str],
+            linestyles: tuple[str, str],
             trial_folder_path: str,
             show_graph: bool = True
     ) -> None:
         """
         Main interface to generate, display, and save the training history plots.
 
-        Orchestrates the creation of 4 subplots (Avg Loss, RMSE, R2, MAE),
-        populates them with data, saves the figure, and optionally shows the window.
-
         Args:
-            histories (tuple): A tuple containing the 4 history pairs (Train/Val) for
-                               Avg Loss, RMSE, R2, and MAE.
-            plot_colors (tuple): Colors for training and validation lines.
-            plot_linestyles (tuple): Line styles for training and validation lines.
+            train_time (tuple): Duration (H, M, S) to display in the figure title.
+            histories (tuple): A tuple containing the 4 history pairs (Train/Val).
+            colors (tuple): Colors for training and validation lines.
+            linestyles (tuple): Line styles for training and validation lines.
             trial_folder_path (str): The folder path to save the output image.
             show_graph (bool): Whether to open the interactive Matplotlib window.
         """
 
-        # 1. Unpack the histories.
-        avg_loss_history, rmse_loss_history, r2_loss_history, mae_loss_history = histories
+        # 1. Unpack the training time.
+        train_hour, train_min, train_sec = train_time
 
-        # 2. Define the axes.
-        avg_loss_ax: Axes = self.fig.add_subplot(self.gs[0, 0])
-        rmse_loss_ax: Axes = self.fig.add_subplot(self.gs[0, 1])
-        r2_loss_ax: Axes = self.fig.add_subplot(self.gs[1, 0])
-        mae_loss_ax: Axes = self.fig.add_subplot(self.gs[1, 1])
+        # 2. Define the figure and gridspace.
+        fig: Figure = plt.figure(figsize=(10, 10))
+        fig.suptitle(f"Training Time: {int(train_hour):02d}:{int(train_min):02d}:{int(train_sec):02d}")
 
-        # 3. Plot the losses.
-        self._plot_subplot(
-            ax = avg_loss_ax,
-            data = avg_loss_history,
-            colors = plot_colors,
-            linestyles = plot_linestyles,
+        gs = GridSpec(
+            nrows = 2,
+            ncols = 2,
+            figure = fig
+        )
+        # Adjust the spacing between the subplots.
+        gs.update(wspace=0.3)
+
+        # 3. Define the axes.
+        mse_ax: Axes = fig.add_subplot(gs[0, 0])
+        rmse_loss_ax: Axes = fig.add_subplot(gs[0, 1])
+        r2_loss_ax: Axes = fig.add_subplot(gs[1, 0])
+        mae_loss_ax: Axes = fig.add_subplot(gs[1, 1])
+
+        # 4. Unpack the histories.
+        mse_history, rmse_loss_history, r2_loss_history, mae_loss_history = histories
+
+        # 5. Plot the metrics.
+        self._plot_history_subplot(
+            ax = mse_ax,
+            data = mse_history,
+            colors = colors,
+            linestyles = linestyles,
             label = "Average Loss"
         )
 
-        self._plot_subplot(
+        self._plot_history_subplot(
             ax = rmse_loss_ax,
             data = rmse_loss_history,
-            colors = plot_colors,
-            linestyles = plot_linestyles,
+            colors = colors,
+            linestyles = linestyles,
             label = "Root Mean Squared Error Loss"
         )
 
-        self._plot_subplot(
+        self._plot_history_subplot(
             ax = r2_loss_ax,
             data = r2_loss_history,
-            colors = plot_colors,
-            linestyles = plot_linestyles,
+            colors = colors,
+            linestyles = linestyles,
             label = "Coefficient of Determination Loss"
         )
 
-        self._plot_subplot(
+        self._plot_history_subplot(
             ax = mae_loss_ax,
             data = mae_loss_history,
-            colors = plot_colors,
-            linestyles = plot_linestyles,
+            colors = colors,
+            linestyles = linestyles,
             label = "Mean Absolute Error Loss"
         )
 
-        # 4. Save the graphs.
-        self._save_plots(trial_folder_path=trial_folder_path)
+        # 6. Save the graphs.
+        self._save_graph(
+            trial_folder_path = trial_folder_path,
+            image_name = "loss_histories"
+        )
 
-        # 5. If so, show the graph.
+        # 7. If so, show the graph.
+        if show_graph:
+            plt.show()
+
+    def plot_test_results(
+            self,
+            scaling_factors: tuple[NDArray, NDArray],
+            data: tuple[NDArray, NDArray],
+            colors: tuple[str, str],
+            linestyles: tuple[str, str],
+            trial_folder_path: str,
+            show_graph: bool = True
+    ) -> None:
+        """
+        Plots the model predictions against the ground truth targets for the test set.
+
+        Denormalizes the data using the provided scaling factors (min/max) to show
+        real-world units (Capacity in Ah).
+
+        Args:
+            scaling_factors (tuple): (min_val, max_val) used during training normalization.
+            data (tuple): (predictions, targets) normalized arrays.
+            colors (tuple): Colors for prediction and target lines.
+            linestyles (tuple): Line styles for prediction and target lines.
+            trial_folder_path (str): Directory to save the output graph.
+            show_graph (bool): Whether to display the plot window.
+        """
+
+        # 1. Unpack the parameters.
+        min_val, max_val = scaling_factors
+        pred_data, target_data = data
+        pred_color, target_color = colors
+        pred_linestyle, target_linestyle = linestyles
+
+        # 2. Define a figure, gridspace and axis.
+        fig: Figure = plt.figure(figsize=(5, 5))
+        gs = GridSpec(
+            nrows = 1,
+            ncols = 1,
+            figure = fig
+        )
+        ax: Axes = fig.add_subplot(gs[0, 0])
+
+        # 3. Create the x-axis vector.
+        sample_index: NDArray = self._generate_x_axis_range(data=pred_data)
+
+        # 4. Scale the test data back to it's original form.
+        scaled_pred_data: NDArray = pred_data * (max_val - min_val) + min_val 
+        scaled_target_data: NDArray = target_data * (max_val - min_val) + min_val
+
+        # 5. Plot predictions against the targets.
+        pred_line, = ax.plot(
+            sample_index,
+            scaled_pred_data,
+            color = pred_color,
+            label = "Predictions",
+            linestyle = pred_linestyle
+        )
+
+        target_line, = ax.plot(
+            sample_index,
+            scaled_target_data,
+            color = target_color,
+            label = "Targets",
+            linestyle = target_linestyle
+        )
+
+        # 6. Define the settings for the axis.
+        ax.set_title("Prediction against Target Capacities")
+        ax.set_xlabel("Sample Index")
+        ax.set_ylabel("Capacity (Ah)")
+        ax.grid(visible=True)
+        ax.legend(handles=[pred_line, target_line])
+
+        # 7. Add a cursor to the graph.
+        mplcursors.cursor([pred_line, target_line])
+
+        # 8. Save the graph.
+        self._save_graph(
+            trial_folder_path = trial_folder_path,
+            image_name = "pred_vs_target_capacities"
+        )
+
+        # 9. If so, save the graph.
         if show_graph:
             plt.show()
