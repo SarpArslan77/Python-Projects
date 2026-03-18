422703257acfba18eb92996ba9ed8eae9aaa8890
SarpArslan77 <157630184+SarpArslan77@users.noreply.github.com>
2026-03-18 23:25:23 +0300

    Enhance main.py with new visualizers and data plotting
    Updated the main simulation file to include additional visualizers and data plotting functionality.

--- Artificial Intelligence/Reinforcement Learning/Artificial Life Simulation/main.py
+++ Artificial Intelligence/Reinforcement Learning/Artificial Life Simulation/main.py
@@ -12,6 +12,113 @@
 
 #! PW: Possibly wrong.
 
+# 1. Imports classes.
+import pygame
+
+from simulation_visualization import (
+    ConfigSimulationVisualizer,
+    SimulationVisualizer
+)
+from simulation_resources import (
+    ConfigResourceManager,
+    ResourceManager
+)
+from agent import(
+    ConfigAgentManager,
+    AgentManager
+)
+from matplotlib_auto_plotter import MatplotlibAutoPlotter
+from data_visualization import (
+    ConfigDataVisualizer,
+    DataVisualizer
+)
+
+if __name__ == "__main__":
+        
+    # 2. Defines constants.
+    SIMULATION_SIZE: int = 100
+    WINDOW_SIZE: int = 800
+    TABLE_UPDATE_FREQ: int = 20
+
+    # 3. Initializes pygame and rich instances.
+    pygame.init()
+
+    # 4. Declares the classes.
+    config_resource_manager = ConfigResourceManager(
+        simulation_size = SIMULATION_SIZE
+    )
+    resource_manager = ResourceManager(
+        config_resource_manager = config_resource_manager
+    )
+
+    #! resource_manager.debug_randomize_resources()
+
+    config_agent_manager = ConfigAgentManager()
+    agent_manager = AgentManager(
+        config_agent_manager = config_agent_manager,
+        resource_manager = resource_manager,
+        simulation_size = SIMULATION_SIZE
+    )
+
+    config_simulation_visualizer = ConfigSimulationVisualizer(
+        simulation_size = SIMULATION_SIZE,
+        window_size = WINDOW_SIZE
+    )
+    simulation_visualizer = SimulationVisualizer(
+        config_simulation_visualizer = config_simulation_visualizer,
+        resource_manager = resource_manager,
+        agent_manager = agent_manager
+    )
+
+    matplotlib_auto_plotter = MatplotlibAutoPlotter()
+
+    config_data_visualizer = ConfigDataVisualizer()
+    data_visualizer = DataVisualizer(
+        config_data_visualizer = config_data_visualizer,
+        matplotlib_auto_plotter = matplotlib_auto_plotter,
+        agent_manager = agent_manager
+    )
+
+    # 5. Creates the starting generation for the simulation.
+    agent_manager.agents_df = agent_manager.create_agents(
+        old_df = agent_manager.agents_df,
+        count = 100,
+        x_positions = None,
+        y_positions = None
+    )
+
+    # 6. Defines starting values for the loop.
+    running: bool = True
+
+    # 7. Main Loop.
+    while running:
+        # A. Checks, whether the simulation is still running.
+        running = simulation_visualizer.simulation_loop()
+
+        # B. Loops the resources.
+        resource_manager.resource_loop()
+
+        # C. Loops the agents.
+        agent_manager.agent_loop(map_temperature=25)
+
+        # D. Updates the stats history.
+        data_visualizer.update_stats_history()
+
+    # 8. Generates the plots for the history trackers.
+    data_visualizer.plot_data()
+# main.py
+
+#! Custom TODO notes:
+#TODO AD: Add docstring.
+#TODO ATH: Add type hint.
+#TODO CTH: Check type hint.
+#TODO FTH: Fix the hardcoding.
+#TODO HPE: Handle possible error.
+#TODO AC: Add comment.
+#TODO AIC: Add input control.
+
+#! PW: Possibly wrong.
+
 import pygame
 
 if __name__ == "__main__":
